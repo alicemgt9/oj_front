7abb40275882075305a191c9658015bce6d7755e
jalice <jhb_cirno_alice@126.com>
2024-07-03 16:31:51 +0800

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
+import ArcoVue from "@arco-design/web-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
-createApp(App).use(store).use(router).mount("#app");
+import "@arco-design/web-vue/dist/arco.css";
+createApp(App).use(store).use(ArcoVue).use(router).mount("#app");
