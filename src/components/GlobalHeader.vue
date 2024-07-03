fe164f0a57f8c134bf73b88c0d73ed554713a93c
jalice <jhb_cirno_alice@126.com>
2024-07-03 16:32:34 +0800

    first commit

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="globalHeader">
+    <a-menu mode="horizontal" :default-selected-keys="['1']">
+      <a-menu-item
+        key="0"
+        :style="{ padding: 0, marginRight: '38px' }"
+        disabled
+      >
+        <div class="title-logo">
+          <img class="logo" src="../assets/logo.png" />
+          <div class="title">byd oj</div>
+        </div>
+      </a-menu-item>
+      <a-menu-item key="1">Home</a-menu-item>
+      <a-menu-item key="2">Solution</a-menu-item>
+      <a-menu-item key="3">Cloud Service</a-menu-item>
+      <a-menu-item key="4">Cooeration</a-menu-item>
+    </a-menu>
+  </div>
+</template>
+
+<script setup lang="ts" />
+
+<style scoped>
+.menu-demo {
+  box-sizing: border-box;
+  width: 100%;
+  padding: 40px;
+  background-color: var(--color-neutral-2);
+}
+
+.title-logo {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: #444;
+  margin-left: 16px;
+}
+
+.logo {
+  height: 48px;
+  width: 48px;
+}
+</style>
